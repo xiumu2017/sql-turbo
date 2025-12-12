58f1c3a45ed7f330bbd686642d5cc981a65a7fcc
zhangdong3 <zhangdong3@sungrowpower.com>
2025-12-12 20:19:45 +0800

    Update

---
+++ tailwind.config.cjs
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './index.html',
+    './src/**/*.{ts,tsx}',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        brand: '#7C4DFF',
+      },
+    },
+  },
+  plugins: [],
+};
